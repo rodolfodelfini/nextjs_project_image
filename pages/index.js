3b26c737e9a80218955e639e138aaece359fb0c8
rodolfodelfini <rodolfodelfini@gmail.com>
2021-02-16 19:33:13 -0300

    css

--- pages/index.js
+++ pages/index.js
@@ -9,23 +9,30 @@ function Index({ itens }) {
     console.log(itens)
     return(
     <div className={styles.container}>
+            
             <div className={styles.BoxMain}>
+            <div><h1>Test Vercel + NextJs + ReactJs</h1></div>
                 {itens.map(registro =>(
-                    <Link href={{ pathname: '/image/[slug]', query: { slug: registro.id }, }} >
                     <div className={styles.BoxImage} key={registro.id}>
-                        <img  className={styles.image} src={registro.previewURL}/>
+                        <div 
+                            className={styles.image} 
+                            style={
+                                { 
+                                    backgroundImage: `url(${registro.webformatURL})`
+                                }
+                                }>
+                        </div>
                         <div className={styles.infoImage}>
-                            <p className={styles.name}>{registro.tags}</p>
+                            <p >{registro.tags}</p>
                         </div>
                     </div>
-                    </Link>
                 ))}
             </div>
 
         <style jsx global>
         { ` 
             body { 
-                background-color: #000;
+                background-color: #e8e7e8;
                 margin: 0px 0px 0px 0px;
             } 
         `} 
