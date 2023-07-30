d42d711b831dae926400ab831d9964f73d39b9ba
NOTEDELL\rodol <rodolfodelfini@gmail.com>
2023-07-30 12:32:58 -0300

    ajuste css

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, {Component, useState, useEffect } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import axios from 'axios';
 import { render } from 'react-dom';
 import Link from "next/link";
@@ -7,44 +7,32 @@ import styles from '../style/styles_index.module.css';
 
 
 function Index({ itens }) {
-    return(
-    <div className={styles.container}>  
-        <div className={styles.BoxMain}>
-            {itens.map(registro =>(
-            <div className={styles.BoxImage} key={registro.id}>
-                <div 
-                    className={styles.image} 
-                    style={
-                        { 
-                            backgroundImage: `url(${registro.webformatURL})`
-                        }
-                        }>
-                </div>
-                <div className={styles.infoImage}>
-                    <p >{registro.tags}</p>
-                </div>
+    return (
+        <div className={styles.container}>
+            <div className={styles.BoxMain}>
+                {itens.map(registro => (
+                    <div className={styles.BoxImage} key={registro.id}>
+                        <div
+                            className={styles.image}
+                            style={
+                                {
+                                    backgroundImage: `url(${registro.webformatURL})`
+                                }
+                            }>
+                        </div>
+                    </div>
+                ))}
             </div>
-            ))}
         </div>
-
-        <style jsx global>
-        { ` 
-            body { 
-                background-color: #e8e7e8;
-                margin: 0px 0px 0px 0px;
-            } 
-        `} 
-        </style>  
-    </div>
     )
 }
 
-Index.getInitialProps = async (context) =>{
+Index.getInitialProps = async (context) => {
     const API_KEY = process.env.API_KEY;
     const response = await axios.get(
         `https://pixabay.com/api/?key=${API_KEY}&order=popular&image_type=photo&pretty=true&page=1`
     );
-    return { itens: response.data.hits}
+    return { itens: response.data.hits }
 };
 
 export default Index
